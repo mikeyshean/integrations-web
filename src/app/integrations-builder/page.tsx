3d8649b917f671105f0370fbdde28710213fdfde
Mikey Shean <mdshean2@gmail.com>
2022-12-24 21:24:11 -0800

    Add api call

--- src/app/integrations-builder/page.tsx
+++ src/app/integrations-builder/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import { useToast } from '@/context/toast'
+import useIntegrations from 'hooks/useIntegrations'
 
 export default function IntegrationsPage() {
     const addToast = useToast()
+    const { integrations } = useIntegrations()
 
     function clickHandler() {
         addToast({
@@ -12,6 +14,15 @@ export default function IntegrationsPage() {
     return (
         <div >
             <button onClick={clickHandler}>Add Toast</button>
+            {
+                integrations && integrations.map((integration, idx) => {
+                    return (
+                        <div key={idx} className="text-blue-500 text-lg">
+                            {integration.name}
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
