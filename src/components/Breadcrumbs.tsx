f13ea027a4fbdad4c371aae4345341e6d78f4e04
Mikey Shean <mdshean2@gmail.com>
2023-01-16 16:22:30 -0800

    Fix flickering by checking isFetching

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,3 +1,4 @@
+import { classNames } from "./utils"
 
 export type PageType = {
   id: number,
@@ -6,7 +7,7 @@ export type PageType = {
   idx: number,
 }
 
-export default function Breadcrumbs({ pages, onClick }: { pages: PageType[], onClick: (idx: number) => void }) {
+export default function Breadcrumbs({ pages, onClick, currentPageId }: { pages: PageType[], onClick: (idx: number) => void, currentPageId: number }) {
   
   return (
     <nav className="flex bg-white" aria-label="Breadcrumb">
@@ -17,21 +18,26 @@ export default function Breadcrumbs({ pages, onClick }: { pages: PageType[], onC
               
               <button
                 onClick={() => onClick(idx)}
-                className="ml-4 text-sm font-medium text-indigo-900 hover:text-indigo-500"
-                aria-current={page.current ? 'page' : undefined}
+                className={classNames(
+                  idx === 0 ? "ml-4" : "",
+                  "text-sm font-medium text-indigo-900 hover:text-indigo-500"
+                )}
+                aria-current={page.id === currentPageId ? 'page' : undefined}
               >
                 {page.name}
               </button>
-              <svg
-                className="h-full w-6 flex-shrink-0 text-indigo-300 "
-                viewBox="0 0 24 44"
-                preserveAspectRatio="none"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-              </svg>
+              { (idx === 0 || page.id !== currentPageId) && 
+                <svg
+                  className="h-full w-6 flex-shrink-0 text-indigo-300 "
+                  viewBox="0 0 24 44"
+                  preserveAspectRatio="none"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+              }
             </div>
           </li>
         ))}
--- src/screens/application/mapper/index.tsx
+++ src/screens/application/mapper/index.tsx
@@ -11,7 +11,7 @@ export default function MapperPage() {
   const [selectedModel, setSelectedModel ] = useState<SelectItem>(EmptySelectItem)
   const [pages, setPages] = useState<PageType[]>([])
   const [currentModel, setCurrentModel] = useState<{id: number, name: string}>({ id: selectedModel.key as number, name: selectedModel.value })
-  const { data: currentModelData } = api.models.useGetItem({id: currentModel.id, enabled: !!currentModel.id})
+  const { data: currentModelData, isFetching } = api.models.useGetItem({id: currentModel.id, enabled: !!currentModel.id})
   const [show, setShow] = useState(true)
 
 
@@ -31,16 +31,13 @@ export default function MapperPage() {
   }
 
   function fieldObjectOnClick(model: {id: number, name: string}) {
-    setPages(prev => {
-      if (prev[prev.length - 1]) {
-        prev[prev.length - 1].current = false
-      }
-      return [...prev, {id: model.id, name: model.name, idx: prev.length, current: true}]
-    })
+    setPages(prev => [...prev, {id: model.id, name: model.name, idx: prev.length, current: true}])
   }
 
   useEffect(() => {
-    setCurrentModel({id: selectedModel.key as number, name: selectedModel.value})
+    if (selectedModel.key) {
+      setCurrentModel({id: selectedModel.key as number, name: selectedModel.value})
+    }
   }, [selectedModel])
   
   useEffect(() => {
@@ -73,23 +70,23 @@ export default function MapperPage() {
           
           {/* Bread Crumbs */}
           <div className={classNames(
-            currentModelData ? "" : "hidden",
+            currentModelData || isFetching ? "" : "hidden",
             "text-center p-2 border border-indigo-700 rounded-lg rounded-b-none border-b bg-indigo-700 text-white"
             )}>
             Model
           </div>
           <div>
             <div className={classNames(
-              currentModelData ? "" : "hidden",
+              currentModelData || isFetching ? "" : "hidden",
               "overflow-x-scroll border-indigo-300 border rounded-lg rounded-t-none border-t-0 mb-2"
               )}>
-              <Breadcrumbs pages={breadcrumbPages()} onClick={handleBreadcrumbOnClick}/>
+              <Breadcrumbs pages={breadcrumbPages()} onClick={handleBreadcrumbOnClick} currentPageId={currentModel.id} />
             </div>
           </div>
 
           
           <div className={classNames(
-            currentModelData ? "" : "hidden",
+            currentModelData || isFetching ? "" : "hidden",
             "text-center p-2 border border-indigo-700 rounded-lg rounded-b-none border-b bg-indigo-700 text-white"
             )}>
             Fields
@@ -97,11 +94,13 @@ export default function MapperPage() {
 
           {/* Field List */}
           <div className={classNames(
-            currentModelData ? "" : "hidden",
+            currentModelData || isFetching ? "" : "hidden",
             "flex-grow flex flex-col overflow-hidden border border-t-0 rounded-lg rounded-t-none border-indigo-300 bg-indigo-50"
             )}>
             <div className="overflow-y-auto py-3">
-              <StackedCards fields={currentModelData?.fields || []} onClick={fieldObjectOnClick} />
+              {currentModelData?.fields && currentModelData.fields.length > 0 && 
+                <StackedCards fields={currentModelData.fields} onClick={fieldObjectOnClick} />
+              }
             </div>
           </div>
         </div>
